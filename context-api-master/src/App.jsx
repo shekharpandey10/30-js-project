c71ae233c721302b0124695e136d2df73a8915d2
shekharpandey10 <sheakherpandey10@gmail.com>
2026-05-12 00:05:01 +0530

    context updated in root file

--- context-api-master/src/App.jsx
+++ context-api-master/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const { theme, setTheme } = useContext(ThemeContext)
   return (
     <div className={`h-screen w-full flex-col flex items-center justify-evenly ${theme?.toLowerCase() === 'dark' ? 'bg-purple-950' : null}`}>
-      <button className='text-[2rem] bg-black rounded-2xl h-8 flex items-center justify-center w-8 text-white' onClick={() => setTheme(prev => prev === 'dark' ? 'light' : 'dark')}>*</button>
+      <button className='text-[2rem] bg-black rounded-2xl flex items-center justify-center p-3 text-white' onClick={() => setTheme(prev => prev === 'dark' ? 'light' : 'dark')}>Theme</button>
       <Login />
     </div>
   )
